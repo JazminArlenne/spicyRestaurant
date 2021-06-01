0000d3d9bc33b78cce8c621ce5c025e380b9735d
JazminArlenne <69172960+JazminArlenne@users.noreply.github.com>
2021-05-31 21:23:13 -0400

    commit 4
    Conexion a base de datos

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 require_once "controllador/plantilla-controllador.php";
 require_once "controllador/formularios.controllador.php";
 
+require_once "modelo/conexion.php";
+
+$conexion = conexion::conectar();
+echo '<pre>'; print_r($conexion); echo "</pre>";
+
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrTraerPlantilla();
