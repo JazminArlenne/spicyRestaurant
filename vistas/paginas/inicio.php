cc1e8329a9ec23fadf9829cae43cf47e27f74ba7
Jazmin <moronjazmin@gmail.com>
2021-10-09 22:05:10 -0400

    commit 10
    actualizacion

--- vistas/paginas/inicio.php
+++ vistas/paginas/inicio.php
@@ -4,6 +4,19 @@
 
     <section class="container-fluid ">
         
+        <div class="container mt-5 pt-3 mb-5 rounded p-5 text-center" style="height: 500px; width: 500px; color:beige; background:#2b2626;">
+            <h5 class="mt-4 ml-5 pl-4 text-white">Email</h5>
+            <input type="input" class=" neo mt-3 p-3" style="height: 50px; width: 300px; color:#ffffff;" >
+
+            
+        </div>
+
+
+
+
+
+
+
         <h2 class="titulo-algunos-platos">| Ensaladas y entrantes</h2>
 
         <div class="container mt-5 mb-5">
