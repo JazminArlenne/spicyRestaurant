c6373501899c0f0239bd81224ba1fb0ba611bad6
Jazmin <moronjazmin@gmail.com>
2022-01-05 18:15:17 -0400

    Cambios nuevos en la pagina de inicio
    Cambios nuevos

--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,104 +1,114 @@
 
 <main>
-    <!--En esta seccion del main se realizara encontraran algunos platos ofrecidos-->
+    <!--En esta seccion del main con la opcion de ver las cartas-->
 
     <section class="container-fluid ">
         
-
-        <h2 class="titulo-algunos-platos">| Ensaladas y entrantes</h2>
-
-        <div class="container mt-5 mb-5">
+        <div class="container-fluid m-0 p-0 mt-4 s-menu">
             <div class="row">
-                <div class="col">
-                    <div class="card bg-secondary ">
-                        <div class="container-fluid" style="width: 100%; height: 370px; background: #ffffff url(resources/img/entrante.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/entrante.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/entrante.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */  background-size: auto; position: relative;">
-                        </div>
-                        <div class="card-body">
-                            <h3 class="card-title">Pinchos a la plancha</h3>
-                            <p>Pimientos rellenos de jamón, pollo, jitomate escalfado, acompañado de unasalsa de azafrán..</p>
-                        </div>
+                <div class="col-3 s-menuc">
+
+                    <div class="s-menucc">
+
+                        <h2>Menu<h2>
+                        <p>Prueba nuestros platos</p>
+
+                        <button>Conoce el menu</button>
                     </div>
+
                 </div>
-                <div class="col">
-                    <div class="card bg-secondary ">
-                        <div class="container-fluid" style="width: 100%; height: 370px; background: #ffffff url(resources/img/ensalada1.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/ensalada1.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/ensalada1.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */  background-size: auto; position: relative;">
-                        </div>
-                        <div class="card-body">
-                            <h3 class="card-title">Ensalita picaten</h3>
-                            <p>Pimientos rellenos de jamón, pollo, jitomate escalfado, acompañado de unasalsa de azafrán..</p>
-                        </div>  
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card bg-secondary ">
-                        <div class="container-fluid" style="width: 100%; height: 370px; background: #ffffff url(resources/img/entrante1.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/entrante1.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/entrante1.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */  background-size: auto; position: relative;">                       
-                        </div>
-                        <div class="card-body">
-                            <h3 class="card-title">Tocinos revueltos</h3>
-                            <p>Pimientos rellenos de jamón, pollo, jitomate escalfado, acompañado de unasalsa de azafrán..</p>
-                        </div>
-                    </div>
-                </div>
+                <div class="col-6 mb-0 s-menuD" ></div>
             </div>
-        </div>           
+
+            <div class="row ">
+                <div class="col-3 n-menu1 n-menu" ></div>
+                <div class="col-3 n-menu2" ></div>
+                <div class="col-3 n-menu3" ></div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col-6 n-menu4 n-menu" ></div>
+                <div class="col-3 n-menu5"></div>
+            </div>
+        </div>
+         
         
     </section>
 
     <!--En esta seccion del main se encuentran la parte de reservas.-->
     
-    <section class="segundo-header mt-0 reserva" style="width: 100%; height: 500px; background: #ffffff url(resources/img/mesas.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/mesas.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ background-attachment: fixed; background-size: cover; position: relative;
+    <section class="segundo-header mt-5 reserva" style="width: 100%; height: 500px; background: #ffffff url(resources/img/mesas.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/mesas.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ background-attachment: fixed; background-size: cover; position: relative;
         ">
+        
+        <div style="background-color: white; padding-top: 2.5%; padding-left: 3%; position: absolute; height:80%; width:40%; right: 4%; top: 10%">
+            <h2>¡Reserva tu mesa!</h2>
+            <div class="reserva-box">
+                
+                <h5>Reserva tu mesa tanto para disfrutar solo o entre amigos.<br>¡No esperes más!</h5>
 
-        <div class="container w-75 texto-reserva">
-            
-            <p class="text-center text-white ">Catering a nuestros clientes y ofrecemos platillos exquisitos y servicio que nos permite tener eventos elegantes cada año. </p>
+                <div class="reserva-info">
+                    <span><a href="tel:809 000 2000">809 000 2000</a></span>
+                    <br>
+                    
+                    <span><a href="mailto:contacto@spicy.com">contacto@spicy.com</a><span>
+                </div>
+
+                <p>Horario de reservas entre 11:00am-6:00pm </p>
+                <button>Reservar</button>
+            </div>
         </div>
-
-        <div class="container w-25 text-center rounded" style="background: black; opacity: 0.9; height: 100px; margin-top: 40px;">
-           <div class="container text-center">
-                <h3 class="textoh3-reserva">¿Aún no haz reservado?</h3>
-            <a class="btn btn-outline-light" href="#">Aparta ahora</a>
-            </div> 
-        </div>        
 
     </section>
 
-
     <!--En esta seccion del main se encuentran las resenas de los clientes.-->
     <section class="titulo-resena-clientes">
-        <h2>| Nuestros clientes</h2>
-        <div class="container text-center ml-5 pl-4">
-            <div class="row ml-5 pl-4 text-center">
-                <div class="col" style="width: 10%;">
-                    <div class="card bg-secondary">
-                        <img class="" src="resources/img/rostro1.jpg">
-                        <div class="contenido-texto-bloque">
-                            <h4>Nombre cliente</h4>
-                            <p>Que dice el cliente sobre nosotros.</p>
+        <h2>| Reseñas</h2>
+        <div class="container text-center ml-5 pl-4" style="">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active mb-5">
+
+                    <div class="d-inline" style="text-align:center; color: white;">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 class="card-title">Card title</h5>
+
                         </div>
                     </div>
                 </div>
-
-                <div class="col" style="width: 10%;">
-                    <div class="card bg-secondary">
-                        <img src="resources/img/rostro2.jpg">
-                        <div class="contenido-texto-bloque">
-                            <h4>Nombre cliente</h4>
-                            <p>Que dice el cliente sobre nosotros.</p>
+                <div class="carousel-item mb-5">
+                    <div class=" d-inline" style="text-align:center; color: white;">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 class="card-title">Card title</h5>
+                            
                         </div>
                     </div>
                 </div>
-
-                <div class="col ">
-                    <div class="card bg-secondary" style="width: 56%;">
-                        <img src="resources/img/rostro3.jpg">
-                        <div class="contenido-texto-bloque">
-                            <h4>Nombre cliente</h4>
-                            <p>Que dice el cliente sobre nosotros.</p>
+                <div class="carousel-item mb-5">
+                    <div class=" d-inline " style="text-align: center; color: white;">
+                        <div class="card-body ">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 class="card-title">Card title</h5>
+                            
                         </div>
                     </div>
                 </div>
-
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
 
             </div>
         </div>
--- app/vistas/plantilla.php
+++ app/vistas/plantilla.php
@@ -17,26 +17,20 @@ session_start();
 
 </head>
 
-<body style="background: black!important;">
-    <header>
-        
+<body class="container-fluid p-0" style="background: black!important;">
 
         <!--Menu principal-->
-        <section>
-            <div class="container-fluid logobig">
-                <img src="resources/img/logobig.png">
+        <section class="sticky-top container-fluid m-0 p-0" style="background: black!important;">
+            <div class=" logobig">
+                <img src="resources/img/logobig.png" style="width: 30%;">
             </div>
 
-            <nav class="navbar navbar-expand-lg navbar-dark menu sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark menu">
                 <div class="container-fluid">
-                   <!-- <img src="resources/img/logo.ico" width="5%" height="5%" alt="">       -->
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
+                   
                     <div class="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul class="navbar-nav contenido-menu">
+                            
                             <li class="nav-item">
                                 <a class="nav-link active hover-3" aria-current="page" href="/">Inicio</a>
                             </li>
@@ -68,17 +62,17 @@ session_start();
 
 
         <!--Aqui se encuentra la segunda seccion del header-->
-        <section class="segundo-header mt-0" style="width: 100%; height: 340px; background: #ffffff url(resources/img/picante.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ background-attachment: fixed; background-size: cover; position: relative;
+        <section class="segundo-header container-fluid m-0 p-0" style=" height: 340px; background: #ffffff url(resources/img/picante.jpg);  /* fallback for old browsers */ background: -webkit-linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* Chrome 10-25, Safari 5.1-6 */ background: linear-gradient(to right, #f56117be, #0b486bdc) url(resources/img/picante.jpg); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */ background-attachment: fixed; background-size: cover; position: relative;
         ">
 
-        <div class="texto-segundo-header pt-3 mb-5">
-            <!--<h1 class="pt-3 mb-5">Spicy Restaurant</h1>-->
-        </div>
+            <div class="texto-segundo-header pt-3 mb-5">
+                <!--<h1 class="pt-3 mb-5">Spicy Restaurant</h1>-->
+            </div>
 
-        <div class="wave" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-4.09,49.83 C219.66,160.36 351.77,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #000;"></path></svg></div>
+            <div class="wave" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-4.09,49.83 C219.66,160.36 351.77,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #000;"></path></svg></div>
 
-    </section>
-    </header>
+        </section>
+    
                 <!--Aqui se encuentra el cuerpo del trabajo-->
 
 
@@ -111,16 +105,16 @@ session_start();
 
         ?>
 
-    <footer class="container-fluid m-0 p-0 informacion text-light">
+        <footer class="container-fluid m-0 p-0 informacion text-light">
 
             <div class="wave-final"><svg viewBox="0 0 500 150" preserveAspectRatio="none" 
                 ><path d="M0.00,49.98 C206.26,154.44 290.34,-13.31 505.36,71.53 L500.00,0.00 L0.00,0.00 Z"
                 style="stroke: none; fill: #000;"></path></svg></div>
 
 
-                <div class="row mt-5 contenido-footer">
-                    <div class="col-sm">
-                    <img src="resources/img/logobig.png" width="80%" height="40%" style="border-radius:10px ;">
+            <div class="row container mt-5 contenido-footer">
+                <div class="col-sm">
+                    <img src="resources/img/logobig.png" width="85%" height="30%" style="border-radius:10px;">
 
                     <div class="footer-info rounded contenedor-footer">
                         <p><span>Direccion:</span></p>
@@ -128,11 +122,10 @@ session_start();
                         <p><span>Telefono:</span> 809-555-1111</p>
                         <p><span>Email:</span> email@spicy.com</p>
                     </div>
-                    
                 </div>
-
+        
                 <div class="col-sm">
-                    <h2>Contactanos</h2>
+                    <h2 class="text-center">Contactanos</h2>
 
                     <div class="container rounded pb-3 contenedor-footer">
                         <form>
@@ -145,6 +138,7 @@ session_start();
                                 <label for="exampleFormControlInput1">Correo Electronico</label>
                                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                             </div>
+                            
                             <div class="form-group">
                                 <label for="exampleFormControlTextarea1">Mensaje</label>
                                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
@@ -153,36 +147,36 @@ session_start();
                             <button class="btn btn-outline-light mt-2" type="button" id="button-addon2">Enviar</button>
                         </form>
                     </div>
-                       
+                            
                 </div>
+
                 <div class="col-sm">
-                    <h2>Newsletter</h2>
+                    <h2 class="text-center">Newsletter</h2>
 
                     <div class="container rounded pb-3 text-white pt-2 contenedor-footer">
                         <p>Unete a nuestro newsletter y recibe correo de las nuevas novedades y promociones que tendremos para ti.</p>
                         <div class="input-group flex-nowrap">
-                          <div class="input-group-prepend">
-                            <span class="input-group-text" id="addon-wrapping">@</span>
-                          </div>
-                          <input type="text" class="form-control" placeholder="Correo Electronico" aria-label="Correo Electronico" aria-describedby="addon-wrapping">
-                          <div class="input-group-append">
-                            <button class="btn btn-dark" type="button" id="button-addon2">Button</button>
-                          </div>
-                        </div>
-
-                        
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping">@</span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Correo Electronico" aria-label="Correo Electronico" aria-describedby="addon-wrapping">
+                            
+                            <div class="input-group-append">
+                                <button class="btn btn-dark" type="button" id="button-addon2">Button</button>
+                            </div>
+                        </div>       
                     </div>
 
                 </div>
             </div>
+        
             <section class="container-fluid"> 
                 <hr>
                 <div class="container text-center pb-2">
                     <p ><span><i class="fab fa-creative-commons"></i></span> 2021 - Spicy Restaurant - Jazmin Moron | Politica de Privacidad </p>
                 </div>
             </section>
- 
+    
         </footer>
-
     </body>
 </html>
